6d5eaf1a5c2cc8298d4c9ebfbd1ae42ee98798e3
s464941 <michal.nowak2001@wp.pl>
2021-10-04 18:21:56 +0200

    comm1

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,14 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
+    final static Logger logger = Logger.getLogger("Class");
+
     public static void main(String [ ] args) {
+
         System.out.println("Hello! Witaj!");
+        logger.info("Helloooo");
     }
 }
